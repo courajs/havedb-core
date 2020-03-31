e7a246013ce361ddf1ceb374856266132eadfa64
Aaron Sikes <aaron@sikes.io>
2020-03-31 12:18:20 -0400

    Extract value hashing to a module

--- src/lib.rs
+++ src/lib.rs
@@ -1,108 +1,18 @@
-use sha3::{Digest, Sha3_256};
 
 #[derive(Debug, Clone, PartialEq, Eq)]
-enum FullValue {
+pub enum FullValue {
     Blob(Vec<u8>),
     Sum(u8, Box<FullValue>),
     Product(Vec<FullValue>),
 }
 
 #[derive(Debug, Clone, PartialEq)]
-enum ValueFragment {
+pub enum ValueFragment {
     Blob(Vec<u8>),
     Sum(u8, Box<ValueFragment>),
     Product(Vec<ValueFragment>),
     Reference(Hash),
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub struct Hash(pub [u8; 32]);
-
-pub trait ContentHashed {
-    fn hash(&self) -> Hash;
-}
-
-impl ContentHashed for FullValue {
-    fn hash(&self) -> Hash {
-        let mut hasher = Sha3_256::new();
-        match self {
-            FullValue::Blob(bytes) => hasher.input(bytes),
-            FullValue::Sum(discriminant, inner) => {
-                let a = [*discriminant];
-                hasher.input(&a);
-                let h = inner.hash();
-                hasher.input(&h.0);
-            },
-            FullValue::Product(inners) => {
-                for inner in inners {
-                    let h = inner.hash();
-                    hasher.input(&h.0);
-                }
-            },
-        }
-        finish(hasher)
-    }
-}
-
-fn finish(hasher: Sha3_256) -> Hash {
-    let mut val = [0; 32];
-    val.copy_from_slice(hasher.result().as_ref());
-    Hash(val)
-}
-
-impl ContentHashed for ValueFragment {
-    fn hash(&self) -> Hash {
-        match self {
-            ValueFragment::Blob(bytes) => {
-                let mut hasher = Sha3_256::new();
-                hasher.input(bytes);
-                finish(hasher)
-            }
-            ValueFragment::Sum(discriminant, inner) => {
-                let mut hasher = Sha3_256::new();
-                let a = [*discriminant];
-                hasher.input(&a);
-                let h = inner.hash();
-                hasher.input(&h.0);
-                finish(hasher)
-            },
-            ValueFragment::Product(inners) => {
-                let mut hasher = Sha3_256::new();
-                for inner in inners {
-                    let h = inner.hash();
-                    hasher.input(&h.0);
-                }
-                finish(hasher)
-            },
-            ValueFragment::Reference(h) => {
-                *h
-            }
-        }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_full_fragment_equivalence() {
-        let blob = FullValue::Blob(vec![1,2,3]);
-        let sum = FullValue::Sum(12, Box::new(blob.clone()));
-        let prod = FullValue::Product(vec![blob.clone(), sum.clone()]);
-
-        let frag = ValueFragment::Product(vec![
-            ValueFragment::Reference(blob.hash()),
-            ValueFragment::Reference(sum.hash()),
-            ValueFragment::Reference(prod.hash()),
-        ]);
-
-        let full = FullValue::Product(vec![
-            blob,
-            sum,
-            prod,
-        ]);
-
-        assert_eq!(frag.hash(), full.hash());
-    }
-}
+mod hashing;
+pub use hashing::*;
